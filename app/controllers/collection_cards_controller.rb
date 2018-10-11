3441ee252c03d843370e657ef9c129014a8885a3
Thomas Morin <thomasmorin1002@yahoo.fr>
2018-10-11 17:54:01 +0200

    Ajout new form pour CollectionCard

--- app/controllers/collection_cards_controller.rb
+++ app/controllers/collection_cards_controller.rb
@@ -1,13 +1,25 @@
 class CollectionCardsController < ApplicationController
   def index
+    @collection_cards = CollectionCard.all
   end
 
   def new
+    @ref_card = RefCard.find(params[:ref_card_id])
+    @collection_card = CollectionCard.new
   end
 
   def create
+    @collection_card = CollectionCard.new(collection_card_params)
+    @collection_card.ref_card = RefCard.find(params[:id])
+    @collection_card.save
   end
 
   def delete
+  end
+
+  private
+
+  def collection_card_params
+    params.require(:collection_card).permit(:reved, :condition, :language)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'collection_cards/index'
-  get 'collection_cards/new'
-  get 'collection_cards/create'
-  get 'collection_cards/delete'
-  get 'ref_cards/show'
-  get 'lists/show'
+
   root to: 'blocs#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :blocs, only: [ :index ]
   resources :lists, only: [ :show ]
-  resources :ref_cards, only: [ :index, :show ]
+  resources :ref_cards, only: [ :index, :show ] do
+    resources :collection_cards, only: [ :index, :new, :create, :delete ]
+  end
 
 end
