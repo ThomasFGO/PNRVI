5e25ca2421a44e2a2b6fc8ce8ddcda3d6098e739
Thomas Morin <thomasmorin1002@yahoo.fr>
2019-02-14 19:59:08 +0100

    good feed for the test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
-#Message.destroy_all
-#Conversation.destroy_all
-#SelectedCard.destroy_all
-#ShopCard.destroy_all
-#SearchCard.destroy_all
-#CollectionCard.destroy_all
+Message.destroy_all
+Conversation.destroy_all
+SelectedCard.destroy_all
+ShopCard.destroy_all
+SearchCard.destroy_all
+CollectionCard.destroy_all
+User.destroy_all
 RefCard.destroy_all
 EnergyType.destroy_all
 List.destroy_all
