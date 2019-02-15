1226e193869e5fce3f01268c31e864439dedc7c7
Thomas Morin <thomasmorin1002@yahoo.fr>
2019-02-15 13:41:44 +0100

    model ref_card updated for good seeds

--- app/models/ref_card.rb
+++ app/models/ref_card.rb
@@ -1,7 +1,7 @@
 class RefCard < ApplicationRecord
   belongs_to :energy_type
   belongs_to :list
-  has_many :collection_cards
-  has_many :search_cards
-  has_many :shop_cards
+  has_many :collection_cards, dependent: :destroy
+  has_many :search_cards2, dependent: :destroy
+  has_many :shop_cards, dependent: :destroy
 end
