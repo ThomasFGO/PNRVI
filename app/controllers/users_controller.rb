b625b969fd4e26beaa251230c621a59e9ff9af24
Thomas Morin <thomasmorin1002@yahoo.fr>
2019-06-15 19:50:49 +0200

    match part done in user#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
         @good_shop_cards << shop_card
       end
     end
+    @good_shop_cards_count_by_user = @good_shop_cards.group_by(&:user_id).transform_values(&:count).sort_by{ |k, v| v }.reverse.to_h
     @pagy, @user_shop_cards = pagy(@user.shop_cards, size: [1,0,0,1])
   end
 end
