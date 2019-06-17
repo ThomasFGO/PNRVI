706526d84487742c510874e846a0ff464c116281
Thomas Morin <thomasmorin1002@yahoo.fr>
2019-06-17 18:55:44 +0200

    method ok in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :unread
+
+  def unread
+    if user_signed_in?
+      @good_conversations = Conversation.where(sender_id: current_user.id).or(Conversation.where(recipient_id: current_user.id))
+      good_messages = Message.where.not(user_id: current_user).joins(:conversation).where(conversation_id: @good_conversations)
+      @unread_count = good_messages.unread_by(current_user).count
+    end
+  end
 
   protected
 
