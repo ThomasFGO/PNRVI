ba1e1f801fa00a446f4d2f234cad3c4b81c487ac
Thomas Morin <thomasmorin1002@yahoo.fr>
2019-07-06 19:27:49 +0200

    validation inscription ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   acts_as_reader
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
