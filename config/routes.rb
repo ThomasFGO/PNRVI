0bdb58bb85f4a236a86bde80e6df9a561edd32d8
Thomas Morin <thomasmorin1002@yahoo.fr>
2020-01-05 11:06:43 +0100

    Update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,23 +3,12 @@ Rails.application.routes.draw do
 
   get 'jap_lists/index'
   devise_for :users
-  #root to: 'lists#index'
   root to: 'lists#index'
   resources :feeds, only: [ :index ]
   resources :users, only: [ :show ]
-  resources :selected_cards, only: [ :index, :create, :destroy ]
   resources :jap_lists, only: [ :index ]
   resources :lists, only: [ :index, :show ]
-
-  resources :ref_cards, :shallow => true, only: [ :index, :show ] do
-    #resources :collection_cards, except: [ :index ]
-    resources :search_cards, except: [ :index ]
-    resources :shop_cards, except: [ :index ]
-  end
-  #resources :collection_cards, only: [ :index ]
-  resources :shop_cards, only: [ :index ]
-  resources :search_cards, only: [ :index ]
-
+  resources :ref_cards, only: [ :index, :show ]
   resources :conversations do
     resources :messages
   end
