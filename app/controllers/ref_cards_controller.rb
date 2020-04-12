9ec51957d63980ea24c777047285aa76589eaf53
Thomas Morin <thomasmorin1002@yahoo.fr>
2020-04-12 21:37:27 +0200

    Delete comments

--- app/controllers/ref_cards_controller.rb
+++ app/controllers/ref_cards_controller.rb
@@ -14,28 +14,6 @@ class RefCardsController < ApplicationController
     filters_params(params).each do |key, value|
       @ref_cards = @ref_cards.public_send("filter_by_#{key}", value) if value.present?
     end
-    # @ref_cards =
-    #   if params[:artist].present? && params[:scope].present? && params[:bloc].present?
-    #     ref_cards.joins(list: :bloc)
-    #       .public_send(params[:scope])
-    #       .where(sql_query, query: "%#{params[:bloc]}%")
-    #       .search_by_artist(params[:artist])
-    #   elsif params[:scope].present? && params[:bloc].present?
-    #     ref_cards.joins(list: :bloc)
-    #       .public_send(params[:scope])
-    #       .where(sql_query, query: "%#{params[:bloc]}%")
-    #   elsif params[:artist].present? && params[:bloc].present?
-    #     ref_cards.joins(list: :bloc)
-    #       .where(sql_query, query: "%#{params[:bloc]}%")
-    #       .search_by_artist(params[:artist])
-    #   elsif params[:artist].present? && params[:scope].present?
-    #     ref_cards.joins(list: :bloc)
-    #       .public_send(params[:scope])
-    #       .search_by_artist(params[:artist])
-    #   elsif params[:artist].present?
-    #     ref_cards.joins(list: :bloc)
-    #       .search_by_artist(params[:artist])
-    #   end
 
     if @ref_cards
      @pagy, @ref_cards = pagy(@ref_cards, size: [1,0,0,1])
