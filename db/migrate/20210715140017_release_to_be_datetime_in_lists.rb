61e505fe54a2459ec241fc7677e7827e1e49f199
Thomas Morin <thomasmorin1002@yahoo.fr>
2021-07-15 16:15:29 +0200

    Change release from text to datetime

--- db/migrate/20210715140017_release_to_be_datetime_in_lists.rb
+++ db/migrate/20210715140017_release_to_be_datetime_in_lists.rb
@@ -1,7 +1,10 @@
 class ReleaseToBeDatetimeInLists < ActiveRecord::Migration[5.2]
   def change
-    change_column :lists, :fr_release, :date
-    change_column :lists, :us_release, :date
-    change_column :lists, :jap_release, :date
+    remove_column :lists, :fr_release
+    add_column :lists, :fr_release, :datetime
+    remove_column :lists, :us_release
+    add_column :lists, :us_release, :datetime
+    remove_column :lists, :jap_release
+    add_column :lists, :jap_release, :datetime
   end
 end
