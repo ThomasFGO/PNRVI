7ff17c19776536eb6d8d7d065c20361c3ea38825
Thomas Morin <thomasmorin1002@yahoo.fr>
2021-07-16 10:51:50 +0200

    Fix migration file name

--- db/migrate/20210716084138_promo_to_be_string_in_lists.rb
+++ db/migrate/20210716084138_promo_to_be_string_in_lists.rb
@@ -1,4 +1,4 @@
-class PromoToBeKindInLists < ActiveRecord::Migration[5.2]
+class PromoToBeStringInLists < ActiveRecord::Migration[5.2]
   def change
     remove_column :lists, :promo
     add_column :lists, :kind, :string
